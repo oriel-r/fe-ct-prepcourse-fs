ba474e54add7cc5b1e2db870200ef8f577dcf654
oriel-r <orielromero97@gmail.com>
2024-02-28 19:16:38 -0300

    Este commit me costo bastnate, renegue con el punto 18

--- M06 Bucles/ejercicios/18.js
+++ M06 Bucles/ejercicios/18.js
@@ -1,7 +1,23 @@
 function productoEntreNúmeros(a, b) {
-  // Dados dos argumentos "a" y "b", devuelve el producto de todos
+  // Dados dos argumentos "a" y "b", devuelve el producto de   e todos
   // los números entre a y b (inclusive).
-  // Tu código:
-}
+    // Tu código:
+    if (typeof a !== "number" || typeof b !== "number") {
+      return "Has ingresado parametros incorrectos";
+    }
+    if (a === 0 || b === 0) {
+      return 0;
+    }
+  var menor = Math.min(a, b);
+  var mayor = Math.max(a, b);
+  var resultado = 1;
+  for (var i = menor; i <= mayor; i++){
+    resultado *= i;
+    }
+    if (resultado === -0) {
+      return 0;
+    }
+    return resultado;
+  }
 
 module.exports = productoEntreNúmeros;
